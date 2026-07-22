bf0e462831f2fae475729e861e541876091e7c2a
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-22 17:52:41 -0300

    feat: adicionar validacao no comando PlaceOrder e queries do Dapper na infraestrutura

---
+++ src/Fcg.Orders.Application/Features/Commands/PlaceOrder/PlaceOrderCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Fcg.Orders.Application.Features.Commands.PlaceOrder
+{
+    public class PlaceOrderCommandValidator : AbstractValidator<PlaceOrderCommand>
+    {
+        public PlaceOrderCommandValidator()
+        {
+            RuleFor(x => x.GamesIds).NotEmpty().WithMessage("A lista de Games adquiridos não pode estar vazia.");
+        }
+    }
+}
---
+++ src/Fcg.Orders.Infrastructure/Queries/DapperHandler/PriceTypeHandler.cs
@@ -0,0 +1,20 @@
+﻿using Dapper;
+using Fcg.Orders.Domain.ValueObject;
+using System.Data;
+
+namespace Fcg.Orders.Infrastructure.Queries.DapperHandler
+{
+    public class PriceTypeHandler : SqlMapper.TypeHandler<Price>
+    {
+        public override void SetValue(IDbDataParameter parameter, Price? value)
+        {
+            parameter.Value = value?.Amount ?? (object)DBNull.Value;
+        }
+
+        public override Price Parse(object value)
+        {
+            var valorStr = Convert.ToDecimal(value?.ToString() ?? "0");
+            return new Price(valorStr);
+        }
+    }
+}
